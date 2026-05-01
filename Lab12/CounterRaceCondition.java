710e045e4cb807304abd10c5f09bff3f31cff3f3
Saonedd <95407002+sabiristaken@users.noreply.github.com>
2026-05-01 14:06:56 +0400

    Add files via upload

---
+++ Lab12/CounterRaceCondition.java
@@ -0,0 +1,29 @@
+public class CounterRaceCondition {
+    static int counter = 0;
+    static final Object lock = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                synchronized (lock) {
+                    counter++;
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                synchronized (lock) {
+                    counter++;
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println("Counter: " + counter); 
+    }
+}
---
+++ Lab12/ThreadBug.java
@@ -0,0 +1,13 @@
+public class ThreadBug {
+
+    public static void main(String[] args) {
+
+        Thread t = new Thread(() -> {
+            System.out.println("Running in another thread");
+        });
+        
+        t.start();
+
+        System.out.println("Main finished");
+    }
+}
